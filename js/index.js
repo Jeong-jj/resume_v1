f266ae37ad6aa47ca4d278ecab02aac0bb73bab7
Jeong-jj <96231175+Jeong-jj@users.noreply.github.com>
2022-02-07 09:52:05 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('.slideInner').slick({
+    autoplay:true,
+    autoplayspeed:500,
+    speed:500,
+    dots:true,
+    prevArrow: '<button class="slick-arrow slick-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><i class="fas fa-angle-right"></i></button>',
+    // pauseOnHover: true,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll:1,
+})
+
+// slide 그래프
